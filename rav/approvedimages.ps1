94be77df45d6c83c456b00630ad25fd167d2a1ff
Ravneet <ravneetkaur899@gmail.com>
2017-06-06 13:37:41 -0700

    to verify the approved amis

---
+++ rav/approvedimages.ps1
@@ -0,0 +1,35 @@
+﻿function get-imageinfo
+{
+param (
+         [Parameter(Mandatory = $true)] [string] $ImageIDq
+         #[Parameter(Mandatory = $true)] [string] $availabilityZone2
+       )
+            
+           $Src = Get-FoldersCreatedToShareResults
+           $DateNTime = Get-Date -Format "dd_MM_yyyy_HH_mm_ss";
+           $ResultFileForAMIInformation = "$($Src)\BlueAMIInformation_ResultFile_" + $DateNTime + ".txt"
+
+            
+       $images = Get-EC2Image -owner self, amazon
+       $fullimages = $images.ImageId
+ #$fullimages
+       
+           foreach ($img in $fullimages)
+
+            {
+             write-host "comparing $img with $ImageIDq"
+                if($img -ne $ImageIDq) 
+                {
+                $value = 1
+                }
+                else
+                {
+                $value = 0
+                 "The Passed AMI Value matches with Image ID " | out-file -Append $ResultFileForAMIInformation
+                break
+                }
+             } 
+          
+             return $value
+
+           }
